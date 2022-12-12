c9005531d611869d1cc9dcf07d923e6303955fac
Your Name <you@example.com>
2022-12-12 18:51:46 -0500

    New styling added

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3003;
-const drinks = require("./models/drinks.js")
+const drinks = require("./models/drinks.js");
+const food = require("./models/foods.js")
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res) {
+    res.render('pages/index');
+  });
 
 app.get("/drinks/", (req, res) =>{
     res.render("drinks_index.ejs", {
@@ -9,24 +16,37 @@ app.get("/drinks/", (req, res) =>{
     })
   })
 
-
-app.get("/", (req, res) =>{
-    res.send("Welcome to the Gitpub App!")
-})
-
-
-app.get("/drinks/", (req, res) => {
+  app.get("/drinks/", (req, res) => {
     res.send(drinks)
   });
 
-
-app.get("/drinks/:id", (req, res) => {
+  app.get("/drinks/:id", (req, res) => {
     //res.send(drinks[req.params.id])
     res.render("drinks_show.ejs", {
       //second param has to be an object
       drink: drinks[req.params.id]
     })
 });
+
+  app.get("/food/", (req, res) =>{
+    res.render("food_index.ejs", {
+      allFoods: food
+    })
+  })
+
+  app.get("/food/", (req, res) => {
+    res.send(food)
+  });
+
+  app.get("/food/:id", (req, res) => {
+    //res.send(drinks[req.params.id])
+    res.render("food_show.ejs", {
+      //second param has to be an object
+      foods: food[req.params.id]
+    })
+});
+
+
 
 
 app.listen(port, () =>{
