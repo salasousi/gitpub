c9e6fd6ea3ce2a13adeda81c467ba2afaf2f3a85
Your Name <you@example.com>
2022-12-12 16:54:08 -0500

    Show route set-up complete

--- server.js
+++ server.js
@@ -17,7 +17,16 @@ app.get("/", (req, res) =>{
 
 app.get("/drinks/", (req, res) => {
     res.send(drinks)
-  })
+  });
+
+
+app.get("/drinks/:id", (req, res) => {
+    res.send(drinks[req.params.id])
+    // res.render("drinks_show.ejs", {
+    //   //second param has to be an object
+    //   drink: drinks[req.params.id]
+    // })
+});
 
 
 app.listen(port, () =>{
